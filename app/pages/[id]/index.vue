ac22386de16100f71450edb9e5463d60ebe19e26
Amir Maghami <Maghami.A1987@gmail.com>
2025-09-14 01:27:19 +0330

    Add transaction form and types to group page
    Integrated fetching and display of users, tags, categories, and transactions for a group. Added a transaction form using the new ITransaction type and transactionSchema. Updated types to include ITransaction.

--- app/pages/[id]/index.vue
+++ app/pages/[id]/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { userSchema } from '~/schemas';
-import type { IGroup, IUser } from '~/types';
+import { transactionSchema, userSchema } from '~/schemas';
+import type { IGroup, IUser, ITransaction } from '~/types';
 
 definePageMeta({
     layout: 'default'
@@ -19,17 +19,63 @@ if (error) {
     groupData.value = data
 }
 const usersData = ref<Partial<IUser>[]>([])
+const { data: users, error: usersError } = await supabase.from('group_users').select('*').eq('group_id', id)
+if (usersError) {
+    toast.add({ title: `Error: ${usersError.code}`, description: usersError.message, color: 'error' })
+} else {
+    usersData.value = users
+}
+
+const { data: tags, error: tagsError } = await supabase.from('tags').select('*').eq('group_id', id)
+const { data: categories, error: categoriesError } = await supabase.from('categories').select('*').eq('group_id', id)
+const { data: transactions, error: transactionsError } = await supabase.from('transactions').select('*').eq('group_id', id)
+
+
+const transactionData = ref<Partial<ITransaction>>({} as ITransaction)
+
 </script>
 <template>
     <section class="py-12">
-     <USkeleton v-if="loading" />
-    <UCard v-else>
-        <h1>{{ groupData.title }}</h1>
-        <p>{{ groupData.description }}</p>
-        <p>{{ groupData.owner_id }}</p>
-        <p>{{ groupData }}</p>
-    </UCard>
+        <USkeleton v-if="loading" />
+        <UCard v-else>
+            <h1>{{ groupData.title }}</h1>
+            <p>{{ groupData.description }}</p>
+            <p>{{ groupData.owner_id }}</p>
+            <p>{{ groupData }}</p>
+        </UCard>
+        <USeparator />
+        <div class="">
+            <h2>Users</h2>
+            <p>{{ usersData }}</p>
+        </div>
+        <USeparator />
+        <div class="">
+            <h2>Tags</h2>
+            <p>{{ tags }}</p>
 
- 
- </section>
+        </div>
+        <USeparator />
+
+        <div class="">
+            <h2>Transactions</h2>
+            <p>{{ transactions }}</p>
+        </div>
+        <UForm :state="transactionData" :schema="transactionSchema"></UForm>
+        <UFormField label="Amount" name="amount">
+            <UInput type="number" v-model="transactionData.amount" :ui="{ root: 'w-full' }" />
+        </UFormField>
+        <UFormField label="Type" name="type">
+            <USelectMenu v-model="transactionData.type" :items="['income', 'expense']" :ui="{ base: 'w-full' }" />
+        </UFormField>
+        <UFormField label="Description" name="description">
+            <UInput v-model="transactionData.description" :ui="{ root: 'w-full' }" />
+        </UFormField>
+        <UFormField label="Category" name="category_id">
+            <USelect v-model="transactionData.category_id" :ui="{ base: 'w-full' }">
+                <USelectOption v-for="category in categories" :key="category.id" :value="category.id">
+                    {{category.name}}
+                </USelectOption>
+            </USelect>
+        </UFormField>
+    </section>
 </template>
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,5 @@
 import { z} from 'zod'
-import { createGroupSchema, groupSchema, loginSchema, registerSchema, updateGroupSchema, userSchema } from '~/schemas'
+import { createGroupSchema, groupSchema, loginSchema, registerSchema, transactionSchema, updateGroupSchema, userSchema } from '~/schemas'
 export type ILogin = z.output<typeof loginSchema>
 
 export type IRegister = z.output<typeof registerSchema>
@@ -8,3 +8,5 @@ export type IGroup = z.output<typeof groupSchema>
 export type ICreateGroup = z.output<typeof createGroupSchema>
 export type IUpdateGroup = z.output<typeof updateGroupSchema>
 export type IUser = z.output<typeof userSchema>
+
+export type ITransaction = z.output<typeof transactionSchema>
